d166382dbfb7f916db5fca23873d44781f704694
arayam <arayam@student.chalmers.se>
2020-11-23 18:55:23 +0100

    interface changes

--- src/main/java/createMember.java
+++ src/main/java/createMember.java
@@ -1,15 +1,18 @@
 public class createMember {
 
+    randomID randID = new randomID();
+
     private String name;
     private String username;
     private String password;
-    private int level;
+    private int level; private int memberKey;
 
     public createMember(String name, String username, String password, int level){
         this.name =name;
         this.username=username;
         this.password=password;
         this.level=level;
+        this.memberKey = randID.getRandom();
     }
 
     public String getName() {
--- src/main/java/createProject.java
+++ src/main/java/createProject.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class createProject {
@@ -5,7 +6,8 @@ public class createProject {
     private String projectName;
     private int weeks;
     private String[][] projectMilestones;
-    private int[] memberKey;
+    private ArrayList<Integer> memberKey;
+    private int projectKey;
 
     public createProject(String projectName, int weeks, int milestones, int task){
         this.projectName=projectName;
@@ -27,7 +29,7 @@ public class createProject {
 
     public void addMemberKey(int key){
 
-
+        memberKey.add(key);
 
     }
 
