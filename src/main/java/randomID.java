1f47db90620936ac12cb25ae6b35b70cfacf5081
arayam <arayam@student.chalmers.se>
2020-11-23 18:47:01 +0100

    interface changes

---
+++ src/main/java/randomID.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+
+
+
+
+    public class randomID {
+
+        private static volatile randomID randID = new randomID();
+
+        private int[][] ID;
+        private int min = 1001;
+        private int max = 9999;
+        private int number;
+
+
+        public static randomID getInstance() {
+            if(randID == null) {
+                randID = new randomID();
+            }
+            return randID;
+        }
+
+        public void generate(){
+            ID = new int[500][2];
+
+            for(int i =0;i<ID.length;i++){
+                ID[i][0] = new Random().nextInt((max-min) + 1) + min;
+                ID[i][1] = 0;
+            }
+        }
+
+        public int getRandom(){
+
+            for(int i=0;i< ID.length;i++){
+                if(ID[i][1] == 0){
+                    this.number = (ID[i][0]);
+                    ID[i][1] = 1;
+                    break;
+                }
+            }
+            return number;
+        }
+
+
+
+    }
+
+
