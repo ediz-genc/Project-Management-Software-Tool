847945355ea525bad8fd0216b51b5f03f1244a58
Patricia Antunes Marklund <antunesp@net.chalmers.se>
2021-01-06 21:36:10 +0100

    Fixed new budget class that wasn't saved in the last push.

---
+++ src/main/java/Projects/Budget.java
@@ -0,0 +1,49 @@
+package Projects;
+
+import java.util.ArrayList;
+
+public class Budget {
+
+    private int totalWorkHours;
+    private int memberCostPerHour;
+    private int amountOfMembers;
+    private int velocity;
+    private int extraCost;
+    ArrayList<Budget> budgetCost = new ArrayList<>();
+
+    public Budget (int totalWorkHours, int memberCostPerHour, int amountOfMembers, int velocity, int extraCost){
+        this.totalWorkHours = totalWorkHours;
+        this.memberCostPerHour = memberCostPerHour;
+        this.amountOfMembers = amountOfMembers;
+        this.velocity = velocity;
+        this.extraCost = extraCost;
+        ArrayList<Budget> budgetCost;
+    }
+
+    public Budget (){
+    }
+
+    public int getTotalWorkHours() { return totalWorkHours; }
+
+    public void setTotalWorkHours(int totalWorkHours) { this.totalWorkHours = totalWorkHours; }
+
+    public int getMemberCostPerHour() { return memberCostPerHour; }
+
+    public void setMemberCostPerHour(int memberCostPerHour) { this.memberCostPerHour = memberCostPerHour; }
+
+    public int getAmountOfMembers() { return amountOfMembers; }
+
+    public void setAmountOfMembers(int amountOfMembers) { this.amountOfMembers = amountOfMembers; }
+
+    public int getVelocity() { return velocity; }
+
+    public void setVelocity(int velocity) { this.velocity = velocity; }
+
+    public int getExtraCost() { return extraCost; }
+
+    public void setExtraCost(int extraCost) { this.extraCost = extraCost; }
+
+    public ArrayList<Budget> getBudgetCost() { return budgetCost; }
+
+    public void setBudgetCost(ArrayList<Budget> budgetCost) { this.budgetCost = budgetCost; }
+}
